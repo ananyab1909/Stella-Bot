87a27d496f5e92e59922557c6255e1d41ea1618a
Ananya Biswas <153214834+ananyab1909@users.noreply.github.com>
2024-08-13 18:25:33 +0530

    Added dependencies

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['TELEGRAM_TOKEN'] = "telegram-token"
+os.environ['WEATHER_API'] = "weather-api-key"
+os.environ['GENAI'] = "gemini-api-key"
+os.environ['NEWS_API'] = "news-api-key"
